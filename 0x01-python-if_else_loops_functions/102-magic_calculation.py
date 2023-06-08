4cceb36fc6bed39909ad13a1ade11fae3d124de0
Adeyemi Oluwagbenga Daniel <adeyemidanielgad@gmail.com>
2023-06-07 17:06:04 -0700

    update

--- 0x01-python-if_else_loops_functions/102-magic_calculation.py
+++ 0x01-python-if_else_loops_functions/102-magic_calculation.py
@@ -1,37 +1,11 @@
-Executable File  34 lines (28 sloc)  677 Bytes
- 
+#!/usr/bin/python3
+# 102-magic_calculation.py
 
-#include "lists.h"
 
-/**
- * insert_node - Inserts a number into a sorted singly-linked list.
- * @head: A pointer the head of the linked list.
- * @number: The number to insert.
- *
- * Return: If the function fails - NULL.
- * Otherwise - a pointer to the new node.
- */
-listint_t *insert_node(listint_t **head, int number)
-{
-	listint_t *node = *head, *new;
-
-	new = malloc(sizeof(listint_t));
-	if (new == NULL)
-		return (NULL);
-	new->n = number;
-
-	if (node == NULL || node->n >= number)
-	{
-		new->next = node;
-		*head = new;
-		return (new);
-	}
-
-	while (node && node->next && node->next->n < number)
-		node = node->next;
-
-	new->next = node->next;
-	node->next = new;
-
-	return (new);
-}
+def magic_calculation(a, b, c):
+    """Match bytecode provided by Holberton School."""
+    if a < b:
+        return (c)
+    if c > b:
+        return (a + b)
+    return (a*b - c)
--- 0x01-python-if_else_loops_functions/13-insert_number.c
+++ 0x01-python-if_else_loops_functions/13-insert_number.c
@@ -32,3 +32,5 @@ listint_t *insert_node(listint_t **head, int number)
 
 	return (new);
 }
+Footer
+© 2023 GitHub, Inc.
